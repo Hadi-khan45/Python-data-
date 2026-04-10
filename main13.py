82b6469c064fa368207dc1d1a308aabed1ddd31e
Hadi-khan45 <hadikhan5493@gmail.com>
2026-04-10 21:18:28 +0500

    Python practice

--- main13.py
+++ main13.py
@@ -13,7 +13,14 @@ finally:
     
 
 print("HEllo World after the error")
-print("2nd exception occurd")  
-print("HEllo World after the error")
-print("HEllo World after the error")
-print("HEllo World after the error")
+
+
+try:
+    # Code that may raise an exception
+    result = int(input("Enter a number: "))
+except ValueError:
+    # Handle ValueError
+    print("Invalid input. Please enter a valid number.")
+except Exception as e:
+    # Handle any other exception
+    print("An error occurred:", e)
