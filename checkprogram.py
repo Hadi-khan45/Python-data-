e473ebe885e1c9bc5af2dead73b7d549f8f69af3
Hadi-khan45 <hadikhan5493@gmail.com>
2026-04-26 19:59:26 +0500

    Python practice

--- checkprogram.py
+++ checkprogram.py
@@ -4,16 +4,3 @@ num=int(input("Enter a number for base :"))
 num2=int(input("Enter a number for power :"))
 output=int(math.pow(num,num2))
 print(f"number = {num} , power = {num2} : ", output)
-        
-    
-
-    
-
-
-
-    
-
-
-
-    
-
